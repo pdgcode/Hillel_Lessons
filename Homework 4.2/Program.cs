9a9a27a3dffc68b9cdd8f11ec3924e1c65aa7cad
Oleksandr <generation.bad.store@gmail.com>
2024-08-06 22:08:24 +0300

    Add project files.

---
+++ Homework 4.2/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Enter the first number");
+double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter operation +, -, *, /");
+string operation = Console.ReadLine();
+
+Console.WriteLine("Enter the second number");
+double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+if (operation == "+")
+{
+    double addition = firstNumber + secondNumber;
+    Console.WriteLine($"{firstNumber} + {secondNumber} = {addition}");
+}
+else if (operation == "-")
+{
+    double subtraction = firstNumber - secondNumber;
+    Console.WriteLine($"{firstNumber} - {secondNumber} = {subtraction}");
+}
+else if (operation == "*")
+{
+    double multiplication = firstNumber * secondNumber;
+    Console.WriteLine($"{firstNumber} * {secondNumber} = {multiplication}");
+}
+else if (operation == "/")
+{
+    if (secondNumber == 0)
+    {
+        Console.WriteLine("It is not possible to divide by 0");
+    }
+    else
+    {
+        double division = firstNumber / secondNumber;
+        Console.WriteLine($"{firstNumber} / {secondNumber} = {division}");
+    }
+
+}
+
---
+++ Homework_4.1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Enter the number of the day of the week: ");
+string numberOfDay = Console.ReadLine();
+switch (numberOfDay)
+{
+    case "1":
+        Console.WriteLine("It's Monday");
+        break;
+    case "2":
+        Console.WriteLine("It's Tuesday ");
+        break;
+    case "3":
+        Console.WriteLine("It's Wednesday");
+        break;
+    case "4":
+        Console.WriteLine("It's Thursday");
+        break;
+    case "5":
+        Console.WriteLine("It's Friday");
+        break;
+    case "6":
+        Console.WriteLine("It's Saturday");
+        break;
+    case "7":
+        Console.WriteLine("It's Sunday");
+        break;
+}

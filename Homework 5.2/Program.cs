1c1a9237dbe5388a2897957d29d118c50c8e95f1
Oleksandr <generation.bad.store@gmail.com>
2024-08-10 00:27:19 +0300

    Add homework 5.2

---
+++ Homework 5.2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework_5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть число: ");
+
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Ви ввели число {number}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Це не число");
+            }
+
+        }
+    }
+}

52542fdfa29b58b1c5a1a7a980f3ed2461ae80a0
Oleksandr <generation.bad.store@gmail.com>
2024-08-10 00:21:03 +0300

    Add homeword 5.1

---
+++ Hillel_Lesson/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Hillel_Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int zero = 0;
+                int k = 5 / zero;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Неможливо поділити на нуль");
+            }
+        }
+    }
+}
---
+++ Homework 5.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Channels;
+
+namespace Homework_5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum;
+            Console.WriteLine("Введіть перше число");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть друге число");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                sum = firstNumber / secondNumber;
+                Console.WriteLine($"{firstNumber} / {secondNumber} = {sum}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Не можна поділити на нуль");
+                   
+            }
+
+
+        }
+    }
+}

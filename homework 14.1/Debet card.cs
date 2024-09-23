861a450ef281beab75e20daf1bdb226d5511ffd0
Oleksandr <generation.bad.store@gmail.com>
2024-09-23 19:04:44 +0300

    add homework 14.1

---
+++ homework 14.1/Debet card.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_account
+{
+    public class Account
+    {
+        public string nameCard;
+        public double amountOfMoney;
+        public Account(string nameCard, double amountOfMoney)
+        {
+            this.nameCard = nameCard;
+            this.amountOfMoney = amountOfMoney;
+        }
+
+        public double deposite(double cash)
+        {
+            return amountOfMoney += cash;
+        }
+
+        public double windrawMoney(double cash)
+        {
+            if (amountOfMoney - cash < 0)
+            {
+                Console.WriteLine("Not enought of money!");
+                return amountOfMoney;
+
+            }
+            else
+            {
+                return amountOfMoney -= cash;
+            }
+        }
+
+        public void viewBalance()
+        {
+            Console.WriteLine($"{nameCard} balance {amountOfMoney:c}");
+        }
+    }
+}
+
---
+++ homework 14.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Bank_account
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Account card = new Account("PrivatBank", 200);
+            Account card2 = new Account("MonoBank", 370.60);
+
+            card.deposite(140.35);
+            Console.WriteLine($"The balans of {card.nameCard} now: {card.amountOfMoney:c}");
+            card2.windrawMoney(60);
+            Console.WriteLine($"The balans of {card2.nameCard} now: {card2.amountOfMoney:c}");
+            card.viewBalance();
+        }
+    }
+}
